d88892a7e754642b90551419a72733e2e84f2c34
Aarthika <arththika04@gmail.com>
2026-02-11 12:43:23 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js"
+import mongoose from 'mongoose' 
 dotenv.config();
+
 const PORT = process.env.PORT;
 // Create an Express app
 const app = express();
@@ -9,7 +11,18 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1/users", userRoutes);
 
+// mongo db connection 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB connected"))
+.catch(err=> {
+  console.error("DB connections Error:",err.message);
+  process.exit(1);
+}
+);
 // Start the server
 app.listen(PORT, () => {
 console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+
